7e14d88389c379e1ee2561a128f126e5c47d1e6b
Sean Brill <seanbrill54@gmail.com>
2024-01-28 23:37:13 -0500

    latest site updates, more coming very soon

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,69 +1,63 @@
-import logo from './logo.svg';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Home from './pages/home/home';
-import ThemeContext from './context/ThemeContext';
-import { useEffect, useState } from 'react';
-import './App.css';
+import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
+import ThemeContext from "./context/ThemeContext";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [initialLoad,setInitialLoad] = useState(true);
-  const [theme,setTheme] = useState('light');
+  const [initialLoad, setInitialLoad] = useState(true);
+  const [theme, setTheme] = useState("dark");
 
   //once per render
-  useEffect(()=>{
-
-    if(localStorage.getItem('theme')==null){
-      console.log('initally saving theme preference: ' + theme);
-      localStorage.setItem('theme',theme);
+  useEffect(() => {
+    if (localStorage.getItem("theme") == null) {
+      console.log("initally saving theme preference: " + theme);
+      localStorage.setItem("theme", theme);
     }
 
-    if(initialLoad && localStorage.getItem('theme')!=null){
-      setTheme(localStorage.getItem('theme'));
+    if (initialLoad && localStorage.getItem("theme") != null) {
+      setTheme(localStorage.getItem("theme"));
       setInitialLoad(false);
     }
     updateSiteTheme();
-  },[theme]);
+  }, [theme]);
 
-  function updateSiteTheme(){
-    var r = document.querySelector(':root');
-    switch(theme){
-      case 'light':
-        r.style.setProperty('--primary-color', '#ffffff');
-        r.style.setProperty('--headshot-background-color','#313232');
-        r.style.setProperty('--secondary-color', '#589ce6');
-        r.style.setProperty('--tertiary-color', '#857e76');
-        r.style.setProperty('--expertise-icon-background-color',"#589ce6");
-        r.style.setProperty('--text-color', '#000');
+  function updateSiteTheme() {
+    var r = document.querySelector(":root");
+    switch (theme) {
+      case "light":
+        r.style.setProperty("--primary-color", "#eae2e2");
+        r.style.setProperty("--headshot-background-color", "#313232");
+        r.style.setProperty("--secondary-color", "#589ce6");
+        r.style.setProperty("--tertiary-color", "#857e76");
+        r.style.setProperty("--expertise-icon-background-color", "#589ce6");
+        r.style.setProperty("--text-color", "#000");
         break;
-      case 'dark':
-        r.style.setProperty('--primary-color', '#202026');
-        r.style.setProperty('--secondary-color', '#ffffff');
-        r.style.setProperty('--headshot-background-color','#ffffff');
-        r.style.setProperty('--tertiary-color', '#6255d1');
-        r.style.setProperty('--expertise-icon-background-color',"#6255d1");
+      case "dark":
+        r.style.setProperty("--primary-color", "#202026");
+        r.style.setProperty("--secondary-color", "#ffffff");
+        r.style.setProperty("--headshot-background-color", "#ffffff");
+        r.style.setProperty("--tertiary-color", "#6255d1");
+        r.style.setProperty("--expertise-icon-background-color", "#6255d1");
 
-        r.style.setProperty('--text-color', '#dddddd');
+        r.style.setProperty("--text-color", "#dddddd");
         break;
     }
     //save theme in local storage
-    if(!initialLoad){
-      console.log('saving theme preference: ' + theme);
-      localStorage.setItem('theme',theme);
+    if (!initialLoad) {
+      console.log("saving theme preference: " + theme);
+      localStorage.setItem("theme", theme);
     }
-    
-    
-    
-
   }
 
   return (
-    <ThemeContext.Provider value={{theme,setTheme}}>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-          </Routes>
-        </BrowserRouter>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeContext.Provider>
   );
 }
--- portfolio-website/src/components/navbar/navbar.jsx
+++ portfolio-website/src/components/navbar/navbar.jsx
@@ -1,95 +1,88 @@
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import ThemeContext from "../../context/ThemeContext";
-import logo from '../../assets/images/SeanLogo.png';
-import moon from '../../assets/images/moon.png';
-import sun from '../../assets/images/sun.png';
-import './navbar.css';
+import logo from "../../assets/images/SeanLogo.png";
+import moon from "../../assets/images/moon.png";
+import sun from "../../assets/images/sun.png";
+import "./navbar.css";
 
-function Navbar(){
-    const {theme,setTheme} = useContext(ThemeContext);
-    const [navOpen,setNavOpen] = useState(false);
+function Navbar() {
+  const { theme, setTheme } = useContext(ThemeContext);
+  const [navOpen, setNavOpen] = useState(false);
 
-    //once per load
-    useEffect(()=>{
-
-        if(navOpen){
-            console.log('open');
-            document.querySelector('#nav-shelf').style.maxHeight = '200px';
-            document.querySelector('#nav-shelf').style.paddingTop = '20px';
-            document.querySelector('#nav-shelf').style.paddingBottom = '20px';
-        }else{
-            console.log('closed');
-            document.querySelector('#nav-shelf').style.maxHeight = '0px';
-            document.querySelector('#nav-shelf').style.paddingTop = '0px';
-            document.querySelector('#nav-shelf').style.paddingBottom = '0px';
-        }
-
-    },[navOpen]);
-
-    function toggleTheme(){
-        if(theme == 'light'){
-            setTheme('dark');
-        }else{
-            setTheme('light');
-        }
+  //once per load
+  useEffect(() => {
+    if (navOpen) {
+      console.log("open");
+      document.querySelector("#nav-shelf").style.maxHeight = "200px";
+      document.querySelector("#nav-shelf").style.paddingTop = "20px";
+      document.querySelector("#nav-shelf").style.paddingBottom = "20px";
+    } else {
+      console.log("closed");
+      document.querySelector("#nav-shelf").style.maxHeight = "0px";
+      document.querySelector("#nav-shelf").style.paddingTop = "0px";
+      document.querySelector("#nav-shelf").style.paddingBottom = "0px";
     }
+  }, [navOpen]);
 
-
-    function toggleNav(){
-        let toggle = document.querySelector('#nav-toggle');
-        toggle.classList.toggle('open');
-        setNavOpen(!navOpen);
+  function toggleTheme() {
+    if (theme == "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
     }
+  }
 
+  function toggleNav() {
+    let toggle = document.querySelector("#nav-toggle");
+    toggle.classList.toggle("open");
+    setNavOpen(!navOpen);
+  }
 
-    return(
+  return (
     <nav id="nav" className="navbar">
-        <section className="nav-container">
-            <section className="nav-left">
-                <div className="logo-container">
-                    <img className="logo-image" src={logo} alt="sean logo" />
-                </div>
-                <section className="nav-title-section">
-                        <p className="title-name">Sean Brill,</p>
-                        <p className="title-position">Software Engineer</p>
-                </section>
-            </section>
-            
-            <section className="links-section">
-            <div className="theme-button"  onClick={toggleTheme}>
-                    {theme == 'light' ?
-                    <img className="theme-image" src={moon} alt="Dark Theme" />
-                        :
-                    <img className="theme-image" src={sun} alt="Light Theme" /> 
-                    }
-                </div>
-                <a className="nav-link active">About Me</a>
-                <a className="nav-link">Projects</a>
-            </section>
-
-            <section className="responsive-links">
-                    <button  id="nav-toggle" className="nav-toggle" onClick={toggleNav}>
-                        <div className="nav-toggle-brgr">
-                        </div>
-                    </button>
-            </section>
+      <section className="nav-container">
+        <section className="nav-left">
+          <div className="logo-container">
+            <img className="logo-image" src={logo} alt="sean logo" />
+          </div>
+          <section className="nav-title-section">
+            <p className="title-name">Sean Brill,</p>
+            <p className="title-position">Software Engineer</p>
+          </section>
         </section>
 
-        <section id="nav-shelf" className="nav-shelf">
-                <a className="nav-link active">About Me</a>
-                <a className="nav-link">Projects</a>
-                <div className="theme-button"  onClick={toggleTheme}>
-                    {theme == 'light' ?
-                    <img className="theme-image" src={moon} alt="Dark Theme" />
-                        :
-                    <img className="theme-image" src={sun} alt="Light Theme" /> 
-                    }
-                </div> 
+        <section className="links-section">
+          <div className="theme-button" onClick={toggleTheme}>
+            {theme == "light" ? <img className="theme-image" src={moon} alt="Dark Theme" /> : <img className="theme-image" src={sun} alt="Light Theme" />}
+          </div>
+          {/* 
+          <a className="nav-link active">About Me</a>
+          <a className="nav-link">Projects</a>
+          */}
         </section>
+
+        <section className="responsive-links">
+          <button id="nav-toggle" className="nav-toggle" onClick={toggleNav}>
+            <div className="nav-toggle-brgr"></div>
+          </button>
+        </section>
+      </section>
+
+      <section id="nav-shelf" className="nav-shelf">
+        {/* 
+        <a className="nav-link active">About Me</a>
+        <a className="nav-link">Projects</a>
+        */}
+        
+        <a className="nav-link">Links Coming Soon</a>
+
+        <div className="theme-button" onClick={toggleTheme}>
+          {theme == "light" ? <img className="theme-image" src={moon} alt="Dark Theme" /> : <img className="theme-image" src={sun} alt="Light Theme" />}
+        </div>
+      </section>
     </nav>
-    )
-
+  );
 }
 
 export default Navbar;
